e476c47884f120b530f04aae71973401b0c2f632
Paige Eaton <paige@devcollaborative.com>
2019-05-03 08:36:39 -0400

    router and countroller

---
+++ src/Controller/NodeRedirectController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\ssi_events\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\node\Controller\NodeViewController;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+
+
+/**
+ * Custom node redirect controller
+ * Based on: https://www.drupal.org/docs/8/api/routing-system/altering-existing-routes-and-adding-new-routes-based-on-dynamic-ones
+ * And: https://drupal.stackexchange.com/questions/215141/take-over-display-of-a-content-type-node-route/215149#215149
+*/
+class NodeRedirectController extends NodeViewController {
+
+  public function view(EntityInterface $node, $view_mode = 'full', $langcode = NULL) {
+
+    // If this is an event and the 'link directly to url' field is checked,
+    // then redirect to URL
+    $redirect = FALSE;
+    if ($node->getType() == 'event') {
+      $checkbox = $node->get('field_link_directly_to_event_url')->getValue();
+      $url = $node->get('field_url')->getValue();
+
+      if ($checkbox['0']['value'] == 1 && !empty($url)) {
+          $url = $url['0']['uri'];
+          $redirect = TRUE;
+      }
+    }
+
+    if ($redirect == TRUE) {
+      return new TrustedRedirectResponse($url, 302);
+    }
+    // Otherwise just go to the full node
+    else {
+      return parent::view($node, $view_mode, $langcode);
+    }
+  }
+}
---
+++ src/Routing/RouteSubscriber.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\ssi_events\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection) {
+
+    // Alter the canonical node route to our custom route
+    if ($route = $collection->get('entity.node.canonical')) {
+      $route->setDefault('_controller', '\Drupal\ssi_events\Controller\NodeRedirectController::view');
+    }
+  }
+}
